2c77d6d197e3025e1fdd73e3ba5fc7f5d1933851
Amera Belay <amera.belay@osi.ca.gov>
2019-03-28 13:35:05 -0700

    removed dob and client_age from AssessmentHelper test where they were not needed

--- app/javascript/Application/components/Assessment/AssessmentHelper.test.js
+++ app/javascript/Application/components/Assessment/AssessmentHelper.test.js
@@ -573,8 +573,6 @@ describe('AssessmentHelper', () => {
         id: 12345,
         status: 'COMPLETED',
         event_date: '2019-01-01',
-        dob: '2016-01-01',
-        client_age: currentClientAge('2016-01-01'),
         can_release_confidential_info: true,
         conducted_by: 'John Doe',
         state: {
@@ -598,8 +596,6 @@ describe('AssessmentHelper', () => {
         preceding_assessment_id: 12345,
         status: 'IN_PROGRESS',
         event_date: '2019-03-26',
-        dob: '2016-01-01',
-        client_age: 3,
         can_release_confidential_info: false,
         state: {
           domains: [{ items: [{}, {}, {}] }, { items: [{}] }],
@@ -615,8 +611,6 @@ describe('AssessmentHelper', () => {
         id: 12345,
         status: 'COMPLETED',
         event_date: '2019-03-26',
-        dob: '2010-02-01',
-        client_age: currentClientAge('2010-02-01'),
         can_release_confidential_info: true,
         conducted_by: 'John Doe',
         state: {
@@ -638,8 +632,6 @@ describe('AssessmentHelper', () => {
         preceding_assessment_id: 12345,
         status: 'IN_PROGRESS',
         event_date: '2019-03-26',
-        dob: '2010-02-01',
-        client_age: 9,
         can_release_confidential_info: false,
         state: {
           domains: [{ items: [{}, {}, {}] }, { items: [{}] }],
