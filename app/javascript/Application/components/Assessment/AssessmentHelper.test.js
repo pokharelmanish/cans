f0a7c8654dd9b90a8df4491507caed5ce929f69c
Amera Belay <amera.belay@osi.ca.gov>
2019-03-28 13:53:07 -0700

    removed unused currentClientAge import from test

--- app/javascript/Application/components/Assessment/AssessmentHelper.test.js
+++ app/javascript/Application/components/Assessment/AssessmentHelper.test.js
@@ -19,7 +19,6 @@ import {
   hasOneCompletedForReassessment,
   AssessmentType,
   preparePrecedingAssessment,
-  currentClientAge,
   containsNotReviewedDomains,
   createRatingsMap,
 } from './AssessmentHelper'
